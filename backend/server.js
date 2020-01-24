96d064767ce966c0872d181a877ad4370784438c
metalysm <burak.demir@unisign.com.tr>
2020-01-24 16:34:38 +0300

    initial

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const http = require("http").createServer();
+const io = require("socket.io")(http);
+
+http.listen(3000);
+
+let count = 0;
+let activeColor = "#f1f1f1";
+
+io.on("connection", socket => {
+  console.log("new user");
+
+  count++;
+
+  socket.emit("changeColor", activeColor);
+  io.emit("newUser", count); // socket.emit() olsa idi sadece o anki bağlantı yapan kişiye
+                    //emit işlemi yap demekti. io ' da herkese gönderiyoruz
+
+  socket.on("changeColor", color => {
+    activeColor = color;
+    socket.broadcast.emit("changeColor", color);
+  });
+
+  socket.on("disconnect", () => {
+    count--;
+    io.emit("disUser", count);
+    console.log("user disconnected");
+  });
+});
